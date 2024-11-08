cc23c58b667c27bc447f23d3a7b935ece70c9b93
vzutti111 <58186931+vzutti111@users.noreply.github.com>
2024-11-08 23:23:43 +0530

    Delete GroceryApplication directory

--- GroceryApplication/src/main/java/com/grocery/app/repository/GroceryItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.grocery.app.repository;
-
-
-import com.grocery.app.model.GroceryItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroceryItemRepository extends JpaRepository<GroceryItem, Long> {}
